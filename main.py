13765c673833cf36863664e21eb7ec12781bcc74
rpw1134 <ryanw1134@gmail.com>
2025-12-22 23:08:44 -0500

    just setting up and getting used to uv

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from llm-inference-server!")
+from fastapi import FastAPI
+from uvicorn import run
 
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return "Hello, World!"
+    
+
+def main():
+    print("Starting the FastAPI server...")
+    run(app="main:app", port=8000, reload=True)
 
 if __name__ == "__main__":
     main()
