49bf45adad439ab37ea8bba43b50841309605f9e
rpw1134 <ryanw1134@gmail.com>
2025-12-25 00:17:59 -0500

    just returning a response instead of a string

--- src/llm_inference_server/services/chat_service.py
+++ src/llm_inference_server/services/chat_service.py
@@ -1,6 +1,6 @@
-from llama_cpp import Any, Dict, Llama
+from llama_cpp import Any, Dict, Llama, CreateChatCompletionResponse
 from llm_inference_server.types.model import ModelRequest, ModelParams
-from typing import Generator, Iterable, cast, cast
+from typing import Generator, Iterable, Iterator, cast, cast
 
 def get_pretrained_model_instance(repo_id: str, filename: str, model_params: ModelParams) -> Llama:
     model = Llama.from_pretrained(repo_id=repo_id, filename=filename, **model_params.__dict__)
@@ -10,7 +10,7 @@ def get_local_model_instance(model_path: str, model_params: ModelParams) -> Llam
     model = Llama(model_path=model_path, **model_params.__dict__)
     return model
 
-def make_model_request(model_request: ModelRequest) -> str | Generator:
+def make_model_request(model_request: ModelRequest) -> CreateChatCompletionResponse | Generator:
     # Get model instance based on local or pretrained
     model = get_pretrained_model_instance(model_request.repo_id, model_request.filename, model_request.model_params) if not model_request.local else get_local_model_instance(model_request.model_path, model_request.model_params)
     
@@ -20,9 +20,12 @@ def make_model_request(model_request: ModelRequest) -> str | Generator:
     # Return response or generator
     return response
 
-def create_chat_completion(model: Llama, prompt: str) -> str:
+def create_chat_completion(model: Llama, prompt: str) -> CreateChatCompletionResponse:
     response = model.create_chat_completion(messages=[{"role": "user", "content": prompt}])
-    return str(response)
+    print(type(response))
+    if isinstance(response, Iterator):
+        raise ValueError("Invalid response from model")
+    return response
 
 def stream_chat_completion(model: Llama, prompt: str) -> Generator:
     stream = cast(Iterable[Dict[str, Any]], model.create_chat_completion(
